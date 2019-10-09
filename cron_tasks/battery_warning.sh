7e9ec0dc3cb7f5fe9e302a3b6d7ee8d2f950118d
Eugene Molotov <em92@users.noreply.github.com>
2019-10-09 15:09:54 +0500

    Теперь на моем экране будет показывать

--- cron_tasks/battery_warning.sh
+++ cron_tasks/battery_warning.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
+export DISPLAY=:0.0
+
 upower -i /org/freedesktop/UPower/devices/battery_BAT0 > /tmp/battery_state
 state=`grep state /tmp/battery_state | awk '{ print $2 }'`
 if [ "$state" == "charging" ]; then
     exit 0
 fi
 perc=`grep percentage /tmp/battery_state | awk '{ print $2 }' | sed 's/[^0-9]*//g'`
-if [ "$perc" -lt "10" ]; then
+if [ "$perc" -lt "20" ]; then
     notify-send -u critical WARNING "Battery low - $perc%"
 fi
