26cc1e33f59afd091197573c574c069eb6f2f306
Eugene Molotov <em92@users.noreply.github.com>
2020-05-21 15:25:14 +0500

    Убрал скрипты, которые давно не использую

--- Videos/download_all.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-shopt -s expand_aliases
-source ~/.bash_aliases
-while read line; do
-    yd $line
-done < "${1:-/dev/stdin}"
--- Videos/playlist_lunduke.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-import requests
-
-r = requests.get("https://feed.eugenemolotov.ru/?action=display&bridge=Youtube&c=UCkK9UDm_ZNrq_rIXCz3xCGA&duration_min=&duration_max=&format=Json")
-for item in r.json()["items"]:
-    print(item["url"])
