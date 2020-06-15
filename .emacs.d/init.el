79868893c4cafcbca448df3efbf3c582df21ef2f
Eugene Molotov <em92@users.noreply.github.com>
2020-06-15 11:14:28 +0500

    Xah Lee скинул хороший метод для переключения буфферов

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -29,6 +29,49 @@
 (setq csv-separators '(","))  ;; csv-align-fields, если че
 
 
+(defun xah-user-buffer-q ()
+  "Return t if current buffer is a user buffer, else nil.
+Typically, if buffer name starts with *, it's not considered a user buffer.
+This function is used by buffer switching command and close buffer command, so that next buffer shown is a user buffer.
+You can override this function to get your idea of “user buffer”.
+version 2016-06-18"
+  (interactive)
+  (if (string-equal "*" (substring (buffer-name) 0 1))
+      nil
+    (if (string-equal major-mode "dired-mode")
+        nil
+      t
+      )))
+
+(defun xah-next-user-buffer ()
+  "Switch to the next user buffer.
+“user buffer” is determined by `xah-user-buffer-q'.
+URL `http://ergoemacs.org/emacs/elisp_next_prev_user_buffer.html'
+Version 2016-06-19"
+  (interactive)
+  (next-buffer)
+  (let ((i 0))
+    (while (< i 20)
+      (if (not (xah-user-buffer-q))
+          (progn (next-buffer)
+                 (setq i (1+ i)))
+        (progn (setq i 100))))))
+
+(defun xah-previous-user-buffer ()
+  "Switch to the previous user buffer.
+“user buffer” is determined by `xah-user-buffer-q'.
+URL `http://ergoemacs.org/emacs/elisp_next_prev_user_buffer.html'
+Version 2016-06-19"
+  (interactive)
+  (previous-buffer)
+  (let ((i 0))
+    (while (< i 20)
+      (if (not (xah-user-buffer-q))
+          (progn (previous-buffer)
+                 (setq i (1+ i)))
+        (progn (setq i 100))))))
+
+
 ;; Спасибо этому ресурсу: https://emacsredux.com/blog/2013/04/02/move-current-line-up-or-down/
 (defun move-line-up ()
   "Move up the current line."
@@ -76,8 +119,8 @@
   (define-key map (kbd "C-k") 'kill-whole-line)
   (define-key map (kbd "C-u") 'yank)
   (define-key map (kbd "C-o") 'save-buffer)
-  (define-key map (kbd "M-,") 'previous-buffer)
-  (define-key map (kbd "M-.") 'next-buffer)
+  (define-key map (kbd "M-,") 'xah-previous-user-buffer)
+  (define-key map (kbd "M-.") 'xah-next-user-buffer)
   (define-key map (kbd "C-w") 'isearch-forward)
   (define-key map (kbd "C-r") 'find-file)
   (define-key map (kbd "C-_") 'goto-line)
