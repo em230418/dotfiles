edfeeca6bd35b141f9f48044b602432ef90914bf
Eugene Molotov <em92@users.noreply.github.com>
2021-05-28 14:19:07 +0500

    emacs: для php ввел tab-width чтобы нормально табы выполнялись для rss-bridge

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -272,6 +272,7 @@ Version 2016-06-19"
  'php-mode-hook
  (lambda()
    (auto-complete-mode 1)
+   (tab-width 4)
    (set-my-binds php-mode-map)
    (define-key php-mode-map (kbd "C-p") 'ac-php-find-symbol-at-point)
    (define-key php-mode-map (kbd "C-M-p") 'ac-php-location-stack-back)
