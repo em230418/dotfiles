4592d27a940352e585ea60551a4a0d2e5d8bf5f2
Eugene Molotov <molotov@it-projects.info>
2023-03-13 17:01:10 +0500

    Поправил место для виртуального окружения

--- e/13.bash
+++ e/13.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 source ~/.profile
-source ~/.emacs.d/.python-environments/c13/bin/activate
+source /opt/odoo-projects/13.0/venv/bin/activate
 set-title 13.0
 cd /opt/odoo-projects/13.0/projects
 export HISTFILE=~/e/bash_history/13
