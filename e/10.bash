5e37d9fe42b85c1c7671e5d99ddda20f8706024c
Eugene Molotov <molotov@it-projects.info>
2023-03-19 21:15:09 +0500

    указали правильно расположение для виртуального окружения

--- e/10.bash
+++ e/10.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 source ~/.profile
-source ~/.emacs.d/.python-environments/c10/bin/activate
+source /opt/odoo-projects/10.0/venv/bin/activate
 set-title 10.0
-cd ~/Projects/docker-odoo-10
+cd /opt/odoo-projects/10.0/projects
 export HISTFILE=~/e/bash_history/10
