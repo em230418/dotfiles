418adc2f301bf08cebfb6936e4e7183dea09dbc3
Eugene Molotov <molotov@it-projects.info>
2023-11-10 09:44:18 +0500

    Перенастроил окружение для 11.0 и 12.0

--- e/11.bash
+++ e/11.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 source ~/.profile
-source ~/.emacs.d/.python-environments/c11/bin/activate
+source /opt/odoo-projects/11.0/venv/bin/activate
 set-title 11.0
 cd /opt/odoo-projects/11.0/projects
 export HISTFILE=~/e/bash_history/11
--- e/12.bash
+++ e/12.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 source ~/.profile
-source ~/.emacs.d/.python-environments/c12/bin/activate
+source /opt/odoo-projects/12.0/venv/bin/activate
 set-title 12.0
 cd /opt/odoo-projects/12.0/projects
 export HISTFILE=~/e/bash_history/12
